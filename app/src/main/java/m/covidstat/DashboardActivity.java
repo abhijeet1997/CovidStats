9299a02c490f7dff90997025e8a0ea0ecddb3fcc
fahad <mfahad@thoughtworks.com>
2020-04-25 19:59:25 +0530

    Implemented Landing page flow

--- app/src/main/java/m/covidstat/DashboardActivity.java
+++ app/src/main/java/m/covidstat/DashboardActivity.java
@@ -1,16 +1,37 @@
 package m.covidstat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
 
 import m.covidstat.R;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    private CardView statsCard;
+    private AppCompatImageView close;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         setContentView(R.layout.activity_dashboard);
+
+        statsCard = findViewById(R.id.stats_card);
+        close = findViewById(R.id.close);
+
+        close.setOnClickListener(v -> finish());
+
+        statsCard.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
