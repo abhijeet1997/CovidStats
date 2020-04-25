6de83aea1438dc6c23af5ccd13a05b191f425452
fahad <mfahad@thoughtworks.com>
2020-04-25 18:42:54 +0530

    Update launch page

---
+++ app/src/main/java/m/covidstat/DashboardActivity.java
@@ -0,0 +1,16 @@
+package m.covidstat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import m.covidstat.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
